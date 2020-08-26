a034a9de9453f1a2afdf55ecd35c84627f0fa262
Xandra <xandraureta@gmail.com>
2020-08-26 16:53:54 +0800

    Update card form for the style transfer

--- resources/views/styletransferArt.blade.php
+++ resources/views/styletransferArt.blade.php
@@ -1,14 +1,13 @@
-<div id="particles-js"></div>
 @extends('webLayout')
 
 @section('content')
-
+<div id="particles-js"></div>
 <div id="wrapper">
 	<div id="featured-wrapper">
 		<div id="featured" class="extra2 margin-btm tray">
 			<div class="main-title" data-aos="fade-up" data-aos-duration="2000">
                 <h2>Art Style Fusion</h2>
-                    <span class="byline" style="color: white; font-family: 'Major Mono Display', cursive">Fuse your artwork with a Classic Filipino artist's work</span>
+				<span class="byline" style="color: white; font-family: 'Major Mono Display', cursive">Fuse your artwork with a Classic Filipino artist's work</span>
             </div>
 
 				<div class="featured">
@@ -16,7 +15,8 @@
 						<div class="row">
 							<div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12 m-auto">
 								<div class="card shadow">
-									<div class="card-header bg-info text-white">               
+									<div class="card-header bg-transparent" text-danger">
+										<h1> Some header text <h1>
 									</div>
 									<div class="card-body">
 									<!-- print success message after file upload  -->
@@ -30,8 +30,8 @@
 											@endif
 											<div class="form-group" {{ $errors->has('filename') ? 'has-error' : '' }}>
 												<label for="filename">Upload your artwork (.jpeg, .jpg, .png)</label>
-													<input type="file" name="filename" id="filename" class="form-control">
-													<span class="text-danger"> {{ $errors->first('filename') }}</span>
+												<input type="file" name="filename" id="filename" class="form-control">
+												<span class="text-danger"> {{ $errors->first('filename') }}</span>
 											</div>
 											<div>
 												<label>Select Style</label>
@@ -43,7 +43,7 @@
 												</select>
 											</div>
 									</div>
-									<div class="card-footer">
+									<div class="card-footer bg-transparent">
 										<div class="form-group">
 											<button type="submit" class="btn btn-success btn-md"> Fuse my artwork! </button>
 										</div>
@@ -52,10 +52,7 @@
 								</div>
 							</div>
 						</div>
-						
-						echo Form::select('size', array('L' => 'Large', 'S' => 'Small'));
-
-					</div> 
+				</div> 
 					
 		</div>
 	</div>
@@ -66,33 +63,33 @@
 	particlesJS("particles-js", {"particles":{"number":{"value":80,"density":{"enable":true,"value_area":800}},"color":{"value":"#f8c390"},"shape":{"type":"circle","stroke":{"width":0,"color":"#000000"},"polygon":{"nb_sides":6},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":0.5,"random":false,"anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},"size":{"value":3,"random":true,"anim":{"enable":false,"speed":40,"size_min":0.1,"sync":false}},"line_linked":{"enable":true,"distance":150,"color":"#ffffff","opacity":0.4,"width":1},"move":{"enable":true,"speed":6,"direction":"none","random":false,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":1200}}},"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":true,"mode":"grab"},"onclick":{"enable":true,"mode":"repulse"},"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":1}},"bubble":{"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true});var count_particles, stats, update; stats = new Stats; stats.setMode(0); stats.domElement.style.position = 'absolute'; stats.domElement.style.left = '0px'; stats.domElement.style.top = '0px'; document.body.appendChild(stats.domElement); count_particles = document.querySelector('.js-count-particles'); update = function() { stats.begin(); stats.end(); if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) { count_particles.innerText = window.pJSDom[0].pJS.particles.array.length; } requestAnimationFrame(update); }; requestAnimationFrame(update);;
 
 	//----- jQuery Javascript---- //
-// get the select box element and store it as '$selecBox'
-var $selectbox = $("#selectbox");
-// get the image container and store it as '$imageContainer'
-var $imagecontainer = $(".images");
-// get the current image selection
-var selection = $selectbox.data("selected");
+	// get the select box element and store it as '$selecBox'
+	var $selectbox = $("#selectbox");
+	// get the image container and store it as '$imageContainer'
+	var $imagecontainer = $(".images");
+	// get the current image selection
+	var selection = $selectbox.data("selected");
 
-// listen for when the selection changes...
-// when it does change, run our `changeImageSelection` function
-$selectbox.on("change", changeImageSelection);
+	// listen for when the selection changes...
+	// when it does change, run our `changeImageSelection` function
+	$selectbox.on("change", changeImageSelection);
 
-function changeImageSelection() {
-  // change the `selection` variable to the selected option
-  selection = $selectbox.val();
-  // add a '.loading' class to the $imageContainer
-  $imagecontainer.addClass("loading");
-  // clear the $imageContainer's selected option
-  $imagecontainer[0].dataset.selected = null;
+	function changeImageSelection() {
+		// change the `selection` variable to the selected option
+		selection = $selectbox.val();
+		// add a '.loading' class to the $imageContainer
+		$imagecontainer.addClass("loading");
+		// clear the $imageContainer's selected option
+		$imagecontainer[0].dataset.selected = null;
 
-  // set a timout of 1.5 seconds
-  setTimeout(function() {
-    // remove the '.loading' class from $imageContainer
-    $imagecontainer.removeClass("loading");
-    // add the currently selected option to $imageContainer
-    $imagecontainer[0].dataset.selected = selection;
-  }, 1500);
-}
+		// set a timout of 1.5 seconds
+		setTimeout(function() {
+			// remove the '.loading' class from $imageContainer
+			$imagecontainer.removeClass("loading");
+			// add the currently selected option to $imageContainer
+			$imagecontainer[0].dataset.selected = selection;
+		}, 1500);
+	}
 
 // ----- Plain Javascipt ---- //
 // const selectbox = document.getElementById("selectbox");
