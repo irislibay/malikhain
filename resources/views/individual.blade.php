0fe20c911fb7044fc90e322ec9990eb7addaefdd
JDNBaladjay <smoltortle@gmail.com>
2020-08-31 20:47:46 +0800

    individual page

--- resources/views/individual.blade.php
+++ resources/views/individual.blade.php
@@ -40,11 +40,38 @@
                      </div>
                      <a href="#"> View</a>
                  </div>
-             </div>
+            </div>
+            <div class="card">
+                <img src="https://d32dm0rphc51dk.cloudfront.net/QZfHjRlBWhuRqpLkF_68Gw/large.jpg" class="lebady"/>
+                 <div class="contentbx">
+                     <h2>Paint 4</h2>
+                     <div class="paintName">
+                         <h3>Wave</h3>
+                     </div>
+                     <a href="#"> View</a>
+                 </div>
+            </div>
+        </div>
+        <div class="box4">
+            <div class="card-scroll2">
+                <h2> Pacita Barsana Abad</h2><br>
+                <p> Alma mater: University of the Philippines Diliman, Lone Mountain College/University of San Francisco<br>
+                    Birthdate: October 5 1946<br>
+                    Deathdate: December 7 2004<br>
+                    Age: 58<br>
+                    AKA: -<br>
+                    Spouse: Jack Garrity
+                    </p>
+            </div>
         </div>
         <div class="box3">
             <div class="card-scroll">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum in elit convallis orci condimentum rhoncus. Aliquam dignissim auctor facilisis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Suspendisse dapibus, ex nec ornare dignissim, enim neque eleifend mi, ut fringilla metus odio vel ante. Pellentesque commodo sodales facilisis. Cras mi diam, gravida et vestibulum ac, gravida eget arcu. In vitae ullamcorper augue, vel ornare ante. Phasellus facilisis ante quis lectus faucibus vestibulum. Integer in efficitur tortor, quis accumsan tortor. Sed efficitur velit nec suscipit porta. Aenean blandit justo ac nibh molestie, id facilisis mauris molestie. Aenean viverra sem neque, sit amet eleifend turpis lacinia eu. Nunc rhoncus tortor non lorem lobortis facilisis. Donec a libero vel mauris interdum interdum. Maecenas viverra suscipit erat. Vivamus turpis nibh, iaculis tincidunt dignissim sagittis, vulputate nec ante.</p>
+                <h2> Life of Pacita Abad</h2><br>
+                <p>Both of Abad's parents served, at different times, as Congressman/Congresswomen of Batanes, and Abad earned a BA in political science at the University of the Philippines Diliman in 1967. In 1970, she went to the United States intending to study law, but she instead earned a master's degree in Asian history at Lone Mountain College (which would later become part of the University of San Francisco) in 1972 while supporting herself as a seamstress and a typist. Abad studied painting at the Corcoran School of Art in Washington, D.C. and The Art Students League in New York City. She lived on six continents and worked in more than 50 countries, including Guatemala, Mexico, India, Afghanistan, Yemen, Sudan, Mali, Papua New Guinea, Cambodia, and Indonesia. At the Corcoran School of Art, Abad studied under Berthold Schmutzhart and Blaine Larson, and the two professors helped launch her artistic career.<br><br>
+                During Abad's time in San Francisco's art scene, she married painter George Kleiman, though they later separated. She traveled to art scenes across Asia for a year with Stanford business student Jack Garrity, then returned to the U.S. to study painting, first at the Corcoran School of Art in Washington D.C. and later, at The Art Students League in New York City. While in California, she married Garrity, who became an international development economist.<br><br>
+                Abad created over 4,500 artworks in her career. Her early paintings were primarily figurative socio-political works of people and primitive masks. Another series was large scale paintings of underwater scenes, tropical flowers, and animal wildlife. Pacita's most extensive body of work, however, is her vibrant, colorful abstract work - many very large scale canvases, but also a number of small collages - on a range of materials from canvas and paper to bark cloth, metal, ceramics, and glass. She painted the 55-meter long Alkaff Bridge in Singapore and covered it with 2,350 multicolored circles, just a few months before she died of lung cancer in 2004 in Singapore.<br><br>
+                Abad developed a technique of trapunto painting (named after a quilting technique), which entailed stitching and stuffing her painted canvases to give them a three-dimensional, sculptural effect. She then began incorporating into the surface of her paintings materials such as traditional cloth, mirrors, beads, shells, plastic buttons, and other objects.</p>
+
             </div>
         </div>
 
