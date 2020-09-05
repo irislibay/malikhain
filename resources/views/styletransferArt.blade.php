ccab5863609162f7ae6b9c57a98efa145c54bf2d
jjesuscastro <joesei_castro@dlsu.edu.ph>
2020-09-06 07:01:37 +0800

    Edit styletransferArt blade

--- resources/views/styletransferArt.blade.php
+++ resources/views/styletransferArt.blade.php
@@ -32,29 +32,30 @@
                 <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12 m-auto">
                     <div class="card shadow bg-transparent border border-white text-white mb-5">
                         <div class="mt-4 text-center">
-                        <div class="col-12 row m-auto">
-                            <label class="col-3">
-                                Select Style: 
-                            </label>
-                            <select class="form-control form-control-sm col-5 offset-3">
-
-                                <option value="styles/style-pacitaabad.jpg">
-                                    Pacita Abad
-                                </option>
-
-                                <option value="styles/style-filipinofamily-baldemor.jpg">
-                                    Manuel Baldemor
-                                </option>
-
-                                <option value="styles/style-spolarium-juanluna.jpg">
-                                    Juan Luna
-                                </option>
-
-                                <option value="styles/style-amorsolo-1.jpg">
-                                    Fernando Amorsolo
-                                </option>
-
-                            </select>
+                            <div class="col-12 row m-auto">
+                                <label class="col-3">
+                                    Select Style: 
+                                </label>
+                                <select id="artistSelect" class="form-control form-control-sm col-5 offset-3">
+                                    <option value="/styles/style-pacitaabad.jpg">
+                                        Pacita Abad
+                                    </option>
+                                    <option value="/styles/style-filipinofamily-baldemor.jpg">
+                                        Manuel Baldemor
+                                    </option>
+                                    <option value="/styles/style-spolarium-juanluna.jpg">
+                                        Juan Luna
+                                    </option>
+                                    <option value="/styles/style-amorsolo-1.jpg">
+                                        Fernando Amorsolo
+                                    </option>
+                                </select>
+                                <label class="col-12 my-0">
+                                    Style preview:
+                                </label>
+                                <div class="col-8 border border-white my-3 mx-auto p-0 align-items-center row" style="height: 24em;">
+                                    <img id="artPreview" class="col img-fluid m-auto" src="/styles/style-pacitaabad.jpg" />
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -109,34 +110,37 @@
     <script src="{{ asset('js/particles.js') }}"></script>
 
     <script>
-        //----- jQuery Javascript---- //
-        // get the select box element and store it as '$selecBox'
-        var $selectbox = $("#selectbox");
-        // get the image container and store it as '$imageContainer'
-        var $imagecontainer = $(".images");
-        // get the current image selection
-        var selection = $selectbox.data("selected");
+        $('#artistSelect').change(function(){
+            $('#artPreview').attr('src', $('#artistSelect').val());
+        });
+        // //----- jQuery Javascript---- //
+        // // get the select box element and store it as '$selecBox'
+        // var $selectbox = $("#aristSelect");
+        // // get the image container and store it as '$imageContainer'
+        // var $imagecontainer = $("#artPreview");
+        // // get the current image selection
+        // var selection = $selectbox.data("selected");
 
-        // listen for when the selection changes...
-        // when it does change, run our `changeImageSelection` function
-        $selectbox.on("change", changeImageSelection);
+        // // listen for when the selection changes...
+        // // when it does change, run our `changeImageSelection` function
+        // $selectbox.on("change", changeImageSelection);
 
-        function changeImageSelection() {
-            // change the `selection` variable to the selected option
-            selection = $selectbox.val();
-            // add a '.loading' class to the $imageContainer
-            $imagecontainer.addClass("loading");
-            // clear the $imageContainer's selected option
-            $imagecontainer[0].dataset.selected = null;
+        // function changeImageSelection() {
+        //     // change the `selection` variable to the selected option
+        //     selection = $selectbox.val();
+        //     // add a '.loading' class to the $imageContainer
+        //     $imagecontainer.addClass("loading");
+        //     // clear the $imageContainer's selected option
+        //     $imagecontainer[0].dataset.selected = null;
 
-            // set a timout of 1.5 seconds
-            setTimeout(function() {
-                // remove the '.loading' class from $imageContainer
-                $imagecontainer.removeClass("loading");
-                // add the currently selected option to $imageContainer
-                $imagecontainer[0].dataset.selected = selection;
-            }, 1500);
-        }
+        //     // set a timout of 1.5 seconds
+        //     setTimeout(function() {
+        //         // remove the '.loading' class from $imageContainer
+        //         $imagecontainer.removeClass("loading");
+        //         // add the currently selected option to $imageContainer
+        //         $imagecontainer[0].dataset.selected = selection;
+        //     }, 1500);
+        // }
     </script>
 
 @endsection
