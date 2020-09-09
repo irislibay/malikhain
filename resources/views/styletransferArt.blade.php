9e0115871e3cadfccace7296b0227e1e17caa473
JDNBaladjay <smoltortle@gmail.com>
2020-09-10 00:19:51 +0800

    temp location

--- resources/views/styletransferArt.blade.php
+++ resources/views/styletransferArt.blade.php
@@ -177,7 +177,7 @@
     </script>
 
     <script>
-        var images = ['/../scripts/neural_style/output/1599662194-0.png', '/../scripts/neural_style/output/1599662194-20.png',  '/../scripts/neural_style/output/1599662194-40.png'];
+        var images = ['/output_image/1599662194-0.png', '/output_image/1599662194-200.png',  '/output_image/1599662194-500.png'];
         buildcarousel("output-iteration", images)
         function buildcarousel(id,images){
             var html = $("#"+id).append('<ol class="carousel-indicators"></ol><div class="carousel-inner"></div><a class="carousel-control-prev" href="#'+id+'" role="button" data-slide="prev"><span class="carousel-control-prev-icon" aria-hidden="true"></span><span class="sr-only">Previous</span></a><a class="carousel-control-next" href="#'+id+'" role="button" data-slide="next"><span class="carousel-control-next-icon" aria-hidden="true"></span><span class="sr-only">Next</span></a>');
